97985646a8a2bc45ac826d84bf7aa492504e6281
qiyubrother <qiyubrother>
2019-12-19 22:58:25 +0800

    添加项目文件。

---
+++ FrmMain.cs
@@ -0,0 +1,63 @@
+﻿using PdfiumViewer;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace pdfViewer
+{
+    public partial class FrmMain : Form
+    {
+        public FrmMain()
+        {
+            InitializeComponent();
+        }
+
+        protected override void OnShown(EventArgs e)
+        {
+            base.OnShown(e);
+            pdfViewer1.ShowBookmarks = false;
+            pdfViewer1.ShowToolbar = false;
+            pdfViewer1.ZoomMode = PdfViewerZoomMode.FitWidth;
+
+            OpenFile();
+        }
+
+        private void OpenFile()
+        {
+            using (var form = new OpenFileDialog())
+            {
+                form.Filter = "PDF Files (*.pdf)|*.pdf|All Files (*.*)|*.*";
+                form.RestoreDirectory = true;
+                form.Title = "Open PDF File";
+
+                if (form.ShowDialog(this) != DialogResult.OK)
+                {
+                    Dispose();
+                    return;
+                }
+
+                pdfViewer1.Document?.Dispose();
+                pdfViewer1.Document = OpenDocument(form.FileName);
+            }
+        }
+
+        private PdfDocument OpenDocument(string fileName)
+        {
+            try
+            {
+                return PdfDocument.Load(this, fileName);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(this, ex.Message, Text, MessageBoxButtons.OK, MessageBoxIcon.Error);
+                return null;
+            }
+        }
+    }
+}
